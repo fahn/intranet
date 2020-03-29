0ff76a23f29cc2e204d4a2a3106e108db185701b
Stefan <fahn@users.noreply.github.com>
2020-03-30 01:07:34 +0200

    FIX Security for php 7.2. ADD

--- html/index.php
+++ html/index.php
@@ -6,11 +6,45 @@ if (!defined("BASE_DIR")) {
 
 
 // stage
-$stage = getenv('INTRANET_STAGE', true) ?: getenv('INTRANET_STAGE');
+$stage = getenv('STAGE', true) ?: getenv('STAGE');
 
 
-$url = "/pages";
-header('Location: '. $url);
-exit();
+if($stage == "production") {
+  $url = "/pages";
+  header('Location: '. $url);
+  exit();
+}
 
+
+/************************ TESTING AREA  */
+require_once BASE_DIR .'/vendor/autoload.php';
+use App\Controller\BlogController;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+// init route
+$routes = new RouteCollection();
+
+// route Torunament
+require_once (BASE_DIR .'/inc/html/brdbHtmlTournamentPage.inc.php');
+$routeTournament = new Route('/ttt',
+  ['_controller' => BrdbHtmlTournamentPage::class ]/*,
+  [ 'action'      => '' ],
+  [ 'id'          =>  '[0-9]'] */
+);
+$routes->add('tournament', $routeTournament);
+
+
+// context
+$context = new RequestContext('/');
+
+// Routing can match routes with incoming requests
+$matcher = new UrlMatcher($routes, $context);
+
+
+print_r($matcher);
+print_r($context);
 ?>
